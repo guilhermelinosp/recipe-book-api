c15af70c6a7a27e8e3d7b73959b25e6c4ce4040e
Guilherme Lino <guilhermelinosp@gmail.com>
2023-10-21 10:31:16 -0300

    fix: resolve problem in repo

--- Source/Common/RecipeBook.Domain/Entities/Base.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RecipeBook.Domain.Entities;
-
-public class Base
-{
-    public int Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-}
