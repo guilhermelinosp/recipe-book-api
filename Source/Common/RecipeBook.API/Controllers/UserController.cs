31e4b6c32c0b1b2d6f83e40a85802a67c1de8b61
Guilherme Lino <guilhermelinosp@gmail.com>
2023-10-25 10:28:13 -0300

    feat: add ignore anti forgery token

--- Source/Common/RecipeBook.API/Controllers/UserController.cs
+++ Source/Common/RecipeBook.API/Controllers/UserController.cs
@@ -19,17 +19,16 @@ public class UserController : ControllerBase
         _antiforgery = antiforgery;
     }
 
-    [HttpPost("antiforgery")]
-    public IActionResult AntiForgery()
-    {
-        // Gere o token anti-forgery
-        var tokens = _antiforgery.GetAndStoreTokens(HttpContext);
-        return Ok(new { csrf = tokens.RequestToken });
-    }
+    //[HttpGet("antiforgery")]
+    //public IActionResult AntiForgery()
+    //{
+    //    var tokens = _antiforgery.GetAndStoreTokens(HttpContext);
+    //    return Ok(new { csrf = tokens.RequestToken });
+    //}
 
     [HttpPost("sign-up")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> SignUp([FromBody] SignUpRequest request, [FromHeader(Name = "x-csrf")] string csrfToken)
+    [IgnoreAntiforgeryToken(Order = 10000)]
+    public async Task<IActionResult> SignUp([FromBody] SignUpRequest request)//, [FromHeader(Name = "x-csrf")] string csrfToken)
     {
         await _signUpUseCase.ExecuteAsync(request);
         return Ok();
